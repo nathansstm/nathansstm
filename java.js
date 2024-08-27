7522c857e12a216e7b833daa30de483923fd5210
nathansstm <nathans.stm@yahoo.com>
2024-08-26 22:23:55 -0700

    Update

---
+++ java.js
@@ -0,0 +1,122 @@
+
+
+kie!
+//// iie*
+//// jjd#
+//// ~
+
+//// "#"@#   // SyntaxError: Invalid or unexpected token
+//// )#)#     // SyntaxError: Unexpected token
+//// ?"?_(_"  // SyntaxError: Unexpected token
+//// ;_(_(_())) // SyntaxError: is not defined
+//// ;_(_(_()))
+//// ;_()((_))
+
+// ((_))
+// ))((
+// ()
+
+
+// ;<
+// ''
+// ``
+// ""
+
+// `
+// SyntaxError: Unexpected end of input
+
+// #
+// \
+// SyntaxError: Invalid or unexpected token
+
+
+// /
+// SyntaxError: Invalid regular expression: missing /
+
+
+
+// &&&
+// ...
+// ,,,
+// SyntaxError: Unexpected token
+
+// \\ts.;, . .,,,"'#$+
+// SyntaxError: Invalid or unexpected token
+
+// / djd
+// SyntaxError: Invalid regular expression: missing / 
+
+
+// let a,b;
+// console.log ""value
+// console.log"value";
+// value valuable
+// console.log "value"
+// console.log("");
+// console.log ""
+
+// SyntaxError: Unexpected end of input
+// !
+// +
+// -
+// kaoirnt+
+// jkdkf +
+// yydgu++
+// jjdnd,
+// jjso.
+
+
+// SyntaxError: Unexpected token '''
+// ,
+// ,,
+// ,,,
+// ,,,,
+// , , ,
+// ,jjfni
+// , jdn ,uuj
+// . diid skis
+// .....
+// . .
+// , ,
+// : :
+// , var l;
+//   , ihh
+// If has whitespace and first character is not a letter
+// ,
+// .
+// &
+// ()jjf
+// ?kid
+// )iir // TokenMapping ,.()?:*}]|^%=
+// :iis
+// *kks
+// *
+// ]
+// }
+// |
+// ^
+// %
+// =
+// If has no whitespace and first character is not a letter
+
+
+
+// ReferenceError: kkfj is not defined
+// kkfj, idkh
+// kkdj
+// kkdj , iskk
+// If has whitespace and first character is a letter
+
+
+// SyntaxError: Invalid or unexpected token
+// "
+// '
+// ''"
+
+
+
+
+
+
+
+
